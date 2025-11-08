1f321e74e233246b336b57afcd1e32d6cbe963c0
dvh3152002 <dvh3152002@gmail.com>
2025-11-08 22:48:51 +0700

    tao entity

---
+++ src/TeduBlog.Core/Domain/Content/Series.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace TeduBlog.Core.Domain.Content
+{
+    public class Series
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [MaxLength(250)]
+        [Required]
+        public required string Name { get; set; }
+
+        [Column(TypeName = "varchar(250)")]
+        public required string Slug { get; set; }
+
+        [MaxLength(250)]
+        public string? Description { get; set; }
+        public bool IsActive { get; set; }
+        public int SortOrder {  get; set; }
+        [MaxLength(250)]
+        public string? SeoDescription { get; set; }
+        [MaxLength(250)]
+        public string? Thumbnail { get; set; }
+        public string? Content { get; set; }
+        public Guid OwnerUserId { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
